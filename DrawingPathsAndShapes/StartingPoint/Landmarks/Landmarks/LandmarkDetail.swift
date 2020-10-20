75dac096c2d61a146f33edd57ecf0041e1aaa95a
Vladyslav Fil <wladyslawfil@gmail.com>
2020-10-20 22:31:16 +0300

    Complete Drawing Paths and Shapes

--- DrawingPathsAndShapes/StartingPoint/Landmarks/Landmarks/LandmarkDetail.swift
+++ DrawingPathsAndShapes/StartingPoint/Landmarks/Landmarks/LandmarkDetail.swift
@@ -8,41 +8,43 @@ A view showing the details for a landmark.
 import SwiftUI
 
 struct LandmarkDetail: View {
-    @EnvironmentObject var userData:  UserData
+    @EnvironmentObject var userData: UserData
     var landmark: Landmark
     
     var landmarkIndex: Int {
         userData.landmarks.firstIndex(where: { $0.id == landmark.id })!
     }
-
+    
     var body: some View {
         VStack {
             MapView(coordinate: landmark.locationCoordinate)
                 .edgesIgnoringSafeArea(.top)
                 .frame(height: 300)
-
+            
             CircleImage(image: landmark.image)
                 .offset(x: 0, y: -130)
                 .padding(.bottom, -130)
-
+            
             VStack(alignment: .leading) {
                 HStack {
                     Text(landmark.name)
                         .font(.title)
                     
                     Button(action: {
-                        self.userData.landmarks[self.landmarkIndex].isFavorite.toggle()
+                        self.userData.landmarks[self.landmarkIndex]
+                            .isFavorite.toggle()
                     }) {
-                        if self.userData.landmarks[self.landmarkIndex].isFavorite {
+                        if self.userData.landmarks[self.landmarkIndex]
+                            .isFavorite {
                             Image(systemName: "star.fill")
-                                .foregroundColor(.yellow)
+                                .foregroundColor(Color.yellow)
                         } else {
                             Image(systemName: "star")
-                                .foregroundColor(.gray)
+                                .foregroundColor(Color.gray)
                         }
                     }
                 }
-
+                
                 HStack(alignment: .top) {
                     Text(landmark.park)
                         .font(.subheadline)
@@ -52,15 +54,16 @@ struct LandmarkDetail: View {
                 }
             }
             .padding()
-
+            
             Spacer()
         }
-        .navigationBarTitle(Text(landmark.name), displayMode: .inline)
     }
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail(landmark: landmarkData[0])
+        let userData = UserData()
+        return LandmarkDetail(landmark: userData.landmarks[0])
+            .environmentObject(userData)
     }
 }
